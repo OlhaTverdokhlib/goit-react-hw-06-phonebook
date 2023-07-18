7aa4abf923f7ab9f2b077bf52027c11584577245
Olha Tverdokhlib <balabolya@gmail.com>
2023-07-18 08:27:46 +0200

    fixed App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+
 
 import ContactForm from './ContactForm';
 import Filter from './Filter';
 import ContactList from './ContactList';
 import appStyles from './App.module.css';
-import { addContact } from 'redux/contactSlice/contactSlice';
+
 
 const App = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   
-  const addNewContact = (formData) => {
-    dispatch(addContact(formData));
-  };
+  // const addNewContact = (formData) => {
+  //   dispatch(addContact(formData));
+  // };
 
   
   return (
     <div className={appStyles.container}>
       <h1>Phonebook</h1>
-      <ContactForm addContact={addNewContact} />
+      <ContactForm />
 
       <h2>Contacts</h2>
       <Filter />
